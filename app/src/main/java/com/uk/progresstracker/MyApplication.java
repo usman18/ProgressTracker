a55b813c81f5e5dfeff90e1577c8d2eebafe96a5
Usman <secretdeveloper18@gmail.com>
2018-09-14 18:40:37 +0530

    -> (Realm & Stetho )Dependencies set up and application class added

---
+++ app/src/main/java/com/uk/progresstracker/MyApplication.java
@@ -0,0 +1,29 @@
+package com.uk.progresstracker;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+
+/**
+ * Created by usman on 14-09-2018.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
+
+    }
+}

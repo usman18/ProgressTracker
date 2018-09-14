442ba8fb203fa40636e635a0299fe586eab198d7
Usman <secretdeveloper18@gmail.com>
2018-09-15 00:57:46 +0530

    -> Add Member Activity completed.

--- app/src/main/java/com/uk/progresstracker/AddMemberActivity.java
+++ app/src/main/java/com/uk/progresstracker/AddMemberActivity.java
@@ -1,9 +1,25 @@
 package com.uk.progresstracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.uk.progresstracker.Model.TeamMember;
+
+import io.realm.Realm;
 
 public class AddMemberActivity extends AppCompatActivity {
+
+    private Realm realm;
+
+    private TextInputEditText etName;
+    private TextInputEditText etEid;
+
+    private Button btnAddMember;
 
 
     @Override
@@ -11,7 +27,63 @@ public class AddMemberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
 
+        initialize();
 
+
+
+    }
+
+    private void initialize() {
+
+        realm = Realm.getDefaultInstance();
+
+        etName = findViewById(R.id.member_name);
+        etEid = findViewById(R.id.member_eid);
+
+        btnAddMember = findViewById(R.id.add_member);
+
+        btnAddMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkIfNull()) {
+
+                    if (TextUtils.isEmpty(etName.getText().toString()))
+                        etName.setError("Please enter name");
+
+                    if (TextUtils.isEmpty(etEid.getText().toString()))
+                        etEid.setError("Please enter eid");
+
+                }else {
+
+                    //Submit logic here
+
+                    final TeamMember member = new TeamMember();
+                    member.setEid(etEid.getText().toString().trim());
+                    member.setName(etName.getText().toString().trim());
+
+                    realm.executeTransaction(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            realm.copyToRealmOrUpdate(member);
+                            Log.d("Check","Transaction done");
+                        }
+                    });
+
+
+                }
+
+            }
+        });
+
+    }
+
+    private boolean checkIfNull() {
+
+        if (TextUtils.isEmpty(etName.getText().toString())
+                || TextUtils.isEmpty(etEid.getText().toString()))
+            return true;
+        return false;
 
     }
 }

a4a1ac94992b7a3a47632fa764f7d5ecdc7bacf1
Usman <secretdeveloper18@gmail.com>
2018-09-15 23:54:53 +0530

    -> Members fragment is now getting populated with sorted results.

---
+++ app/src/main/java/com/uk/progresstracker/Adapter/MemberAdapter.java
@@ -0,0 +1,63 @@
+package com.uk.progresstracker.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.uk.progresstracker.Model.TeamMember;
+import com.uk.progresstracker.R;
+
+import io.realm.RealmResults;
+/**
+ * Created by usman on 15-09-2018.
+ */
+
+public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.MemberViewHolder>{
+
+    private Context context;
+    private RealmResults<TeamMember> members;
+
+    public MemberAdapter(Context context, RealmResults<TeamMember> members) {
+        this.context = context;
+        this.members = members;
+    }
+
+    @NonNull
+    @Override
+    public MemberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MemberViewHolder(LayoutInflater.from(context)
+        .inflate(R.layout.member_list,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MemberViewHolder holder, int position) {
+
+        TeamMember member = members.get(position);
+
+        holder.tvName.setText(member.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return members.size();
+    }
+
+
+    class MemberViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvName;
+
+        MemberViewHolder(View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvName);
+
+        }
+    }
+
+}
--- app/src/main/java/com/uk/progresstracker/Fragments/MembersFragment.java
+++ app/src/main/java/com/uk/progresstracker/Fragments/MembersFragment.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.uk.progresstracker.Adapter.MemberAdapter;
+import com.uk.progresstracker.Model.TeamMember;
 import com.uk.progresstracker.R;
+
+import io.realm.OrderedCollectionChangeSet;
+import io.realm.OrderedRealmCollectionChangeListener;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by usman on 14-09-2018.
  */
 
 public class MembersFragment extends Fragment {
+
+    private Realm realm;
+
+    private RecyclerView rvMembers;
+    private MemberAdapter adapter;
+    private RealmResults<TeamMember> realmResults;
 
     @Nullable
     @Override
@@ -26,6 +42,41 @@ public class MembersFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        setUpRealm();
+        initialize(view);
+
+    }
+
+    private void setUpRealm() {
+
+        realm = Realm.getDefaultInstance();
+
+        realmResults = realm.where(TeamMember.class)
+                .findAll().sort("name", Sort.ASCENDING);
+
+        realmResults.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<TeamMember>>() {
+            @Override
+            public void onChange(RealmResults<TeamMember> teamMembers, OrderedCollectionChangeSet changeSet) {
+                changeSet.getChanges();
+                if (adapter != null)
+                    adapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+    }
+
+
+
+    private void initialize(View view) {
+
+        rvMembers = view.findViewById(R.id.rvMembers);
+        rvMembers.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvMembers.setHasFixedSize(true);
+
+        adapter = new MemberAdapter(getContext(),realmResults);
+        rvMembers.setAdapter(adapter);
 
     }
 }
